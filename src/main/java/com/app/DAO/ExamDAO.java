ce5169c0cbd47a3852fec023efe68e9dd108656f
4honestobs <92285499+4honestobs@users.noreply.github.com>
2025-03-18 21:14:24 +0700

    Add, Update, Delete ExamService & ExamDAO

--- src/main/java/com/app/DAO/ExamDAO.java
+++ src/main/java/com/app/DAO/ExamDAO.java
@@ -126,6 +126,60 @@ public class ExamDAO {
 
         return exam;
     }
+    public static boolean addExams(String exOrder, String exCode, String quesIDs) throws SQLException{
 
+        String query = "INSERT INTO exams (exOrder, exCode, ex_quesIDs) VALUES (?,?,?)";
+        try(Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query)){
+            stmt.setString(1, exOrder);
+            stmt.setString(2, exCode);
+            stmt.setString(3, quesIDs);
+
+            int row = stmt.executeUpdate();
+            return row > 0;
+        }
+    }
+    public static boolean deleteExams(String testCode) throws SQLException{
+        String query = "DELETE FROM exams WHERE testCode = ?";
+        //String resetQuery = "SET @num = 0; UPDATE topics SET tpID = @num := (@num+1); ALTER TABLE topics AUTO_INCREMENT = 1;";
+        try(Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query)){
+            stmt.setString(1, testCode);
+            int row = stmt.executeUpdate();
+
+            if (isTableEmpty(connection)) {
+                try (Statement resetStmt = connection.createStatement()) {
+                    resetStmt.execute("ALTER TABLE exams AUTO_INCREMENT = 1;");
+                }
+            }
+            return row > 0;
+        }
+    }
+
+    public static boolean updateExams(String testCode, String exOrder, String exCode, String quesIDs) throws SQLException{
+        String query = "UPDATE exams SET exOrder = ?, exCode = ?, ex_questIDs = ? WHERE testCode = ?";
+
+        try(Connection connection = DatabaseConnection.getConnection();
+            PreparedStatement stmt = connection.prepareStatement(query)){
+
+            stmt.setString(1, exOrder);
+            stmt.setString(2, exCode);
+            stmt.setString(3, quesIDs);
+
+            int row = stmt.executeUpdate();
+            return row > 0;
+        }
+    }
+
+    private static boolean isTableEmpty(Connection connection) throws SQLException {
+        String query = "SELECT COUNT(*) FROM exams";
+        try (Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            if (rs.next()) {
+                return rs.getInt(1) == 0;
+            }
+            return false;
+        }
+    }
 
 }
